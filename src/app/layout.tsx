42c37068d8c4d14fefabe63a24e2c90acdd7a5e0
K.K <gokennsirou@yahoo.co.jp>
2024-06-17 00:50:57 +0900

    コーヒー一覧途中

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import "./globals.css";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} flex flex-col min-h-svh`}>
+        <header className="bg-blue-400">
+          <nav className="container mx-auto p-4">
+            <ul className="flex items-center gap-8">
+              <li>
+                <Link href="/" className="text-lg">
+                  App Router Sample
+                </Link>
+              </li>
+              <li>
+                <Link href="/coffees">Coffees</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main className="container mx-auto p-4 flex-grow">{children}</main>
+        <footer className="bg-blue-400">
+          <div className="container mx-auto p-4">
+            <p className="text-center">
+              &copy; {new Date().getFullYear()} App Router Sample
+            </p>
+          </div>
+        </footer>
+      </body>
     </html>
   );
 }
